47602ca6b23f1448334157cab4fe9acdc5ea803f
gmch1 <1731216262@qq.com>
2020-08-09 15:29:38 +0800

    fix

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8888;
 var app = express();
 // 开启 gzip 压缩
 app.use(compression());
-app.use(express.static("./"));
+app.use(express.static("./dist"));
 module.exports = app.listen(port, function(err) {
   if (err) {
     console.log(err);
